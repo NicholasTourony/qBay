d1ceaf1dd3b1e1d0315e3e146bac28c40a8d9aad
dkasy64 <dawitkasy64@gmail.com>
2024-03-27 19:45:48 -0400

    options were add

--- Run.java
+++ Run.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 public class Run
 {
-    public static void main(String[] args)
-    {
+    public void buy(){
         
+    }
+
+    public static void main(String[] args)
+    { Scanner scan = new Scanner(System.in);
+    
+        System.out.println("what would you like to do\n 1.Buy\n2.Sell\n3.Cart ");
+        int option = scan.nextInt();
+        if(option == 1){
+            buy();
+        }else if (option == 2){
+            sell();
+        }else if( option == 3){
+            cart();     
+        } else{
+            System.out.println("Not a valid option");
+        }
+
     }
 }
